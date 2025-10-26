d2e9462c574d1646fa30ae31d43b7c7f381f39a6
Ash Prasad <ashishpd@hotmail.com>
2025-10-25 20:44:27 -0500

    docs: add environment variable setup instructions for Azure OpenAI integration

--- 08_langchain_open_ai.py
+++ 08_langchain_open_ai.py
@@ -1,3 +1,9 @@
+# ensure the following environment variables are set
+#export OPENAI_API_KEY="xxxx
+#export AZURE_OPENAI_ENDPOINT="https://sxxxx.openai.azure.com/"
+#export OPENAI_API_VERSION="2024-12-01-preview" 
+#export AZURE_OPENAI_DEPLOYMENT_NAME=xxx
+
 from langchain_openai import AzureChatOpenAI
 import os
 
