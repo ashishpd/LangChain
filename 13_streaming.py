562754a061d003fa838cd451b7ca3b8c37cf84d8
Ash Prasad <ashishpd@hotmail.com>
2025-10-28 12:03:16 -0500

    feat: add streaming functionality with ChatOllama for English to Italian translation

---
+++ 13_streaming.py
@@ -0,0 +1,15 @@
+from langchain_core.messages import HumanMessage, SystemMessage
+from langchain_ollama import ChatOllama
+from langchain_core.output_parsers import StrOutputParser
+
+llm = ChatOllama(model="gemma3:270m")
+
+messages = [
+    SystemMessage(content="Translate the following from English into Italian"),
+    HumanMessage(content="hi! how are you?"),
+]
+
+for token in llm.stream(messages):
+    print(token.content, end="|")
+
+
